fcec022f7c33fe28df01684cff527e2f327ea4d6
LJFrancis <joe.mengis@gmail.com>
2020-08-10 16:27:37 -0700

    looks great

---
+++ server.js
@@ -0,0 +1,34 @@
+<<<<<<< Updated upstream
+=======
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+const db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+>>>>>>> Stashed changes
